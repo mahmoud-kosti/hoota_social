68c216ae2acc84018b8bbad30fa16c62b35687fd
Mahmoud Abd allah <21cb538600@gmail.com>
2026-04-15 10:10:49 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const supabaseUrl = "https://xblelhmjgtbobuuntyhq.supabase.co";
-const supabaseKey = "sb_publishable_SPTU7w-HXUQi3pD09uc7fA_bo2_hGxg";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
